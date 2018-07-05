9af314ee5b73801c97a9b2f7cea82337dcbe9478
Maximilian Röll <maximilian.roell@plentymarkets.com>
2018-07-05 21:01:41 +0200

    # Added: * Install script

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+mkdir ~/update-script
+cd ~/update-script
+curl -LkSs https://codeload.github.com/maxiroellplenty/update-script/legacy.zip/master -o master.tar.gz
+mkdir scripts/
+tar -xvzf master.tar.gz -C scripts/
+rm -r master.tar.gz
+cd ~/update-script/scripts
+cd */.
+cp -r /scripts/* ../
+downliadDir=${PWD##*/}
+cd ..
+rm -r "$downliadDir"
